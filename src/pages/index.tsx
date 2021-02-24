4850ccd0e491b3956969ccc688a92d3c82acb4a5
Angelo Rafael <angelorpt@gmail.com>
2021-02-24 18:35:32 -0400

    add | Titulo da Página e Favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,16 @@ import { CompletedChallenges } from '../components/CompleteChallenges'
 import { Countdown } from '../components/Countdown'
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
+
 import styles from '../styles/pages/Home.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | Moviment</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
