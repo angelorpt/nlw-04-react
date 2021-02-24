f45dda63bd758070201bdedf583afbeea6643e33
Angelo Rafael <angelorpt@gmail.com>
2021-02-23 21:22:48 -0400

    add | Component de Profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}> 
+      <img src="https://avatars.githubusercontent.com/u/1192083?s=460&u=38a4381efc8e3bea833141382829ccd4b9561da6&v=4" alt="Foto Perfil" />
+      <div>
+        <strong>Angelo Rafael</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
