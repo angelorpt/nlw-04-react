0cfbca42fcb0b64f4e00c38acaa016346f4cc832
Angelo Rafael <angelorpt@gmail.com>
2021-03-01 10:03:18 -0400

    add | Criado o Contexto de Countdown

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -18,6 +18,7 @@ interface ChallengesContextData {
   levelUp: () => void
   startNewChallenge: () => void
   resetChallenge: () => void
+  completeChallenge: () => void
 }
 
 interface ChallengeProviderProps {
@@ -56,6 +57,26 @@ export function ChallengeProvider({ children } : ChallengeProviderProps) {
     setActiveChallenge(null)
   }
 
+  function completeChallenge() {
+    if (!activeChallenge) {
+      return;
+    }
+
+    const { amount } = activeChallenge
+
+    let finalExperience = currentExperience + amount
+
+    if (finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel
+      levelUp()
+    }
+
+    setCurrentExperience(finalExperience)
+    setActiveChallenge(null)
+    setChallengesCompleted(challengesCompleted + 1)
+
+  }
+
   return (
     <ChallengesContext.Provider 
       value={{
@@ -67,6 +88,7 @@ export function ChallengeProvider({ children } : ChallengeProviderProps) {
         levelUp,
         startNewChallenge,
         resetChallenge,
+        completeChallenge,
       }}
     >
       { children }
