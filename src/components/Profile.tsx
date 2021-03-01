1417c546b66482b58fc3e03858e9fb6c3a815b3c
Angelo Rafael <angelorpt@gmail.com>
2021-03-01 10:02:12 -0400

    fix | Corrigindo a exibição do Level

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export default function Profile() {
+
+  const { level } = useContext(ChallengesContext)
+
   return (
     <div className={styles.profileContainer}> 
       <img src="https://avatars.githubusercontent.com/u/1192083?s=460&u=38a4381efc8e3bea833141382829ccd4b9561da6&v=4" alt="Foto Perfil" />
@@ -8,7 +13,7 @@ export default function Profile() {
         <strong>Angelo Rafael</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
